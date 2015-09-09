f9097f0c996f8acf41f1c08530697e2ab64f81a8
Louis Vichy <lgvichy@gmail.com>
2015-09-09 16:30:01 -0700

    Font and removing the background..

--- client/templates/header.js
+++ client/templates/header.js
@@ -3,19 +3,20 @@ Template.header.events({
   // handle the form submission
   'submit form': function(event) {
     console.log('submit!');
+
     // stop the form from submitting
     event.preventDefault();
 
     // get the data we need from the form
-    var idea = event.target.idea.value
+    var idea = event.target.idea.value;
 
     function showAlert() {
-      $("#success-alert").show();
-          $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
-            //  $("#success-alert").hide();
-            console.log('Done!');
-            event.target.idea.value = '';
-          });
+      $('#success-alert').show();
+      $('#success-alert').fadeTo(2000, 500).slideUp(500, function() {
+        //  $("#success-alert").hide();
+        console.log('Done!');
+        event.target.idea.value = '';
+      });
     }
 
     Meteor.call('addIdea', idea, showAlert);
@@ -32,6 +33,5 @@ Template.header.events({
     //   }
     // });
 
-
-  }
+  },
 });
