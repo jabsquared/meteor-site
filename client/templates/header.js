6b53e25b498014d7e95cdc0b475bf45402e3fd7c
bpshonyak <bpshonyak@live.com>
2015-09-09 12:45:59 -0700

    temp alert doesnt look good atm

---
+++ client/templates/header.js
@@ -0,0 +1,30 @@
+Template.header.events({
+
+  // handle the form submission
+  'submit form': function(event) {
+    console.log('submit!');
+    // stop the form from submitting
+    event.preventDefault();
+
+    // get the data we need from the form
+    var newIdea = {
+      idea: event.target.idea.value,
+      email: event.target.email.value
+    };
+
+    function backUp() {
+      $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+          //  $("#success-alert").hide();
+           console.log('Done!');
+      });
+    }
+
+    // create the new idea
+    // Ideas.insert(newIdea, function(error, _id) {
+    //   if (_id !== null) {
+    // $("#success-alert").show();
+    $("#success-alert").fadeTo(2000, 500).slideDown(5000, backUp);
+    //   }
+    // });
+  }
+});
