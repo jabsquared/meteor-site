330f22cd6bddbcc98128069716256947292c51da
Louis Vichy <lgvichy@gmail.com>
2015-09-09 22:17:57 -0700

    Added further information about the team.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 Meteor.methods({
-  addIdea: function (text, onSuccess) {
+  addIdea: function(text, onSuccess) {
     // Validations go here
 
     Ideas.insert({
       idea: text,
-      createdAt: new Date()
+      createdAt: new Date(),
     }, function(error, _id) {
       if (_id !== null) {
-        if(onSuccess){
+        if (onSuccess) {
           onSuccess();
         }
       }
